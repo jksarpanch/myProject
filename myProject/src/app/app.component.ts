d4fadae88af8755fe63f226906eece599f7fd18d
Sandhu <jksarpanch@gmail.com>
2018-10-28 20:13:43 -0700

    Temp check in

--- myProject/src/app/app.component.ts
+++ myProject/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-
-
+import {Router, Event, NavigationStart, NavigationEnd, NavigationError} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,47 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   currentUrl = '';
 
-  collapedSideBar: boolean;
-
-  constructor (private router: Router) {
+  constructor(private router: Router) {
     router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
         // Show loading indicator
 
-    }
+      }
 
-    if (event instanceof NavigationEnd) {
+      if (event instanceof NavigationEnd) {
         // Hide loading indicator
         this.currentUrl = this.router.url;
-    }
+      }
 
-    if (event instanceof NavigationError) {
+      if (event instanceof NavigationError) {
         // Hide loading indicator
 
         // Present error to user
         console.log(event.error);
-    }
+      }
     });
   }
 
-  title = 'myProject';
-  // tslint:disable-next-line:no-inferrable-types
-  // navOpened: boolean = false;
-  // openNav(): void {
-  //   document.getElementById('mySidenav').style.width = '400px';
-  // }
-  //
-  // closeNav(): void {
-  //   document.getElementById('mySidenav').style.width = '0';
-  // }
-
-  receiveCollapsed($event) {
-    this.collapedSideBar = $event;
+  closeMenu(): void {
+    document.getElementById('mySidenav').style.width = '0';
   }
   ngOnInit() {
     console.log('test2');
     if (this.currentUrl === '/') {
       console.log('test3');
-      }
     }
   }
+}
--- myProject/src/app/header/header.component.ts
+++ myProject/src/app/header/header.component.ts
@@ -1,58 +1,4 @@
-// import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-// import {Router, NavigationEnd} from '@angular/router';
-//
-// @Component({
-//   selector: 'app-header',
-//   templateUrl: './header.component.html',
-//   styleUrls: ['./header.component.scss']
-// })
-// export class HeaderComponent implements OnInit {
-//   pushRightClass: string = 'push-right';
-//   collapedSideBar: boolean;
-//   collapsed: boolean = false;
-//   showMenu: string = '';
-//
-//   @Output() collapsedEvent = new EventEmitter<boolean>();
-//   constructor(public router: Router) {
-//
-//     this.router.events.subscribe(val => {
-//       if (
-//         val instanceof NavigationEnd &&
-//         window.innerWidth <= 992 &&
-//         this.isToggled()
-//       ) {
-//         this.toggleSidebar();
-//       }
-//     });
-//   }
-//
-//   ngOnInit() {
-//   }
-//
-//   isToggled(): boolean {
-//     const dom: Element = document.querySelector('body');
-//     return dom.classList.contains(this.pushRightClass);
-//   }
-//
-//   toggleSidebar() {
-//     const dom: any = document.querySelector('body');
-//     dom.classList.toggle(this.pushRightClass);
-//   }
-//   toggleCollapsed() {
-//     this.collapsed = !this.collapsed;
-//     this.collapsedEvent.emit(this.collapsed);
-//   }
-//
-//   rltAndLtr() {
-//     const dom: any = document.querySelector('body');
-//     dom.classList.toggle('rtl');
-//   }
-//
-//   onLoggedout() {
-//     localStorage.removeItem('isLoggedin');
-//   }
-// }
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -63,8 +9,22 @@ export class HeaderComponent {
   navOpened = false;
 
   @Output() collapsedEvent = new EventEmitter<boolean>();
-  constructor() {}
+  constructor() {
+    this.toggleTitle();
+  }
 
+  toggleTitle() {
+    $('#expandButtons').click(function (e) {
+      e.stopPropagation(); // this stops the event from bubbling up to the body
+    })
+    $(document.body).click( () =>{
+      console.log(document.getElementById('mySidenav').style.width);
+      const currentWidth = document.getElementById('mySidenav').style.width;
+      if (this.navOpened === true) {
+        document.getElementById('mySidenav').style.width = '0';
+      }
+    });
+  }
   openNav(): void {
     document.getElementById('mySidenav').style.width = '400px';
   }
